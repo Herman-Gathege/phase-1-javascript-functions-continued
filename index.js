b349eae6ee2918c82561f67aa08bee4cbc08915c
Herman-Gathege <siinamtechstudio@gmail.com>
2024-07-02 12:28:06 +0300

    addchanges

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+
+
+
+
+
+// def saturdayFun(activity='roller-skate'):
+//     return f"This Saturday, I want to {activity}!"
+
+// def mondayWork(task='go to the office'):
+//     return f"This Monday, I will {task}."
+
+// def wrapAdjective(flair='*'):
+//     def inner(adjective='special'):
+//         return f"You are {flair}{adjective}{flair}!"
+//     return inner
+
+
+
 // code your solution here
+function saturdayFun(activity = 'roller skate') {
+  return (`This saturday i want to, ${activity}`);
+}saturdayFun();
+
+function mondayWork(task = 'go to office') {
+  return  (`this monday i will, ${task}`);
+  
+} mondayWork();
